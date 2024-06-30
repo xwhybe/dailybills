f5743126bc59cc5d0414cedc04601ed0071c97c8
xwhybe <1051750637@qq.com>
2024-06-30 08:31:39 +0800

    delete all files

--- data.cpp
+++
@@ -1,9 +0,0 @@
-#include "data.h"
-
-std::string income[7] = {"lottery", "salary", "part-time job", "borrowed fund", "bonus", "subsidy", "other income"};
-std::string outcome[7] = {"shopping", "food", "car fare", "entertainment", "medical treatment", "education", "other outcome"};
-int rank_now = 1;
-double income_all = 0;
-double outcome_all = 0;
-double net_income = 0;
-std::vector<bill_one> bill_all;
